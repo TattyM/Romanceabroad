9241f12c7bd2616166d7a8c59e6b03658791ed46
TattyM <71784360+TattyM@users.noreply.github.com>
2021-01-11 21:45:28 -0500

    Lesson 27 home task
    Added the dependency in pom.xml
    Added imports manually to test class SingInTests

--- src/test/java/com/romanceabroad/ui/SingInTests.java
+++ src/test/java/com/romanceabroad/ui/SingInTests.java
@@ -1,13 +1,18 @@
 package com.romanceabroad.ui;
 
+import com.automation.remarks.testng.VideoListener;
+import com.automation.remarks.video.annotations.Video;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+
+@Listeners(VideoListener.class)
 public class SingInTests extends BaseUI {
     public static final boolean isTest22 = true;
     String currentUrl;
 
-
+    @Video(name = "SingIn")
     @Test(dataProvider = "SingIn", dataProviderClass = DataProviders.class, priority = 1, enabled = isTest22, groups = {"ie"})
     public void negativeTestsSingIn(String email, String password) {
         mainPage.clickSingInPageLink();
